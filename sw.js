a3f62b46c5f559a2e421319393329222ac841846
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-08 19:33:38 +0300

    Uccf

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v11';
+const CACHE_NAME = 'hasbali-v12';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
