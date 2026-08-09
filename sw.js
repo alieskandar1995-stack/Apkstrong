3044258d5ed610a3236061e8258d6592cdf905fb
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-09 18:04:42 +0300

    Aasd

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v17';
+const CACHE_NAME = 'hasbali-v18';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
