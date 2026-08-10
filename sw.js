805d7e511f5ba5b99b770654444845b31f6049ef
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-10 21:02:26 +0300

    Fdcg

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v23';
+const CACHE_NAME = 'hasbali-v24';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
