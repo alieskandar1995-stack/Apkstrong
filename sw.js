71f3e977828ef46e8fb9c1c568713b2b001c1a66
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-10 20:12:33 +0300

    Update service worker version to v22lpapapp

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v21';
+const CACHE_NAME = 'hasbali-v22';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
