040a5c090526d0c44b616c3939352848b38b074f
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-10 22:56:20 +0300

    Hgfbnhvcllpjg

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v26';
+const CACHE_NAME = 'hasbali-v27';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
