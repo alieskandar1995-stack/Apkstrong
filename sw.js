9bf0e72366557dcbdb3ae0550506ae4bb412089f
alieskandar1995-stack <ali.eskandar1995@gmail.com>
2026-08-14 14:30:30 +0300

    Update service worker to version v11.0.0ffhhgg

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // الإصدار: v11.0.0
 // ============================================================
 
-const CACHE_NAME = 'hasbali-v41';
+const CACHE_NAME = 'hasbali-v42';
 const OFFLINE_URL = '/index.html';
 
 // قائمة الملفات المطلوب تخزينها مسبقاً
